3e4c0ce25f85894a75e1f60a9cdd5e16128b2c8e
JoeBufonidae <joethetoad22@gmail.com>
2025-12-22 13:40:05 -0500

    shouldhideinput not working

--- packages/junon-io/client/src/menus/processor_menu.js
+++ packages/junon-io/client/src/menus/processor_menu.js
@@ -4,31 +4,21 @@ const Item = require("./../entities/item")
 
 class ProcessorMenu extends BaseMenu {
   onMenuConstructed() {
-    this.progressBar = this.el.querySelector(".processor_progress_bar_fill")
   }
 
-  initProcessing() {
-  }
+  initProcessing() {}
 
   initListeners() {
     super.initListeners()
-
-    Array.from(this.el.querySelectorAll(".inventory_slot")).forEach((el) => {
-      this.initInventorySlotListener(el)
-      el.addEventListener("click", this.onInventoryClick.bind(this), true)
-    })
   }
 
   close() {
     this.label = null
-    
     if (!this.game.hideMainMenus) {
       this.showQuickInventory()
     }
-    
     super.close()
   }
-
 
   onInventoryClick(event) {
     this.retrieveInventorySlot(event)
@@ -45,8 +35,11 @@ class ProcessorMenu extends BaseMenu {
   updateStorageInventory(data) {
     super.updateStorageInventory(data)
 
-    let progressWidth = data.progress / 100 * this.getProgressMaxWidth()
-    this.progressBar.style.width = progressWidth + "px"
+    if (this.progressBar) {
+      const progressWidth =
+        (data.progress / 100) * this.getProgressMaxWidth()
+      this.progressBar.style.width = progressWidth + "px"
+    }
   }
 
   getProgressMaxWidth() {
@@ -59,60 +52,64 @@ class ProcessorMenu extends BaseMenu {
 
   open(header, entity, description, shouldHideInput = false, footer = "", options = {}) {
     this.cleanup()
+
     this.storageId = entity.id
-
-    const storageDiv = this.el.querySelector(".processor_storage")
-    storageDiv.innerHTML = ""
-
-    // Determine number of input slots
-    const slotCount = entity.slotCount || (entity.getStorageCount && entity.getStorageCount()) || 1
-
-    // Create input slots dynamically
-    if (!shouldHideInput) {
-      for (let i = 0; i < slotCount; i++) {
-        const slotDiv = document.createElement("div")
-        slotDiv.className = "input_inventory inventory_slot"
-        slotDiv.dataset.index = i
-        slotDiv.innerHTML = "<img src=''>"
-        storageDiv.appendChild(slotDiv)
-      }
+    console.log("this is something triggered here", shouldHideInput)
+    if (shouldHideInput) {
+      this.el.classList.add("processor_output_only")
+    } else {
+      this.el.classList.remove("processor_output_only")
     }
 
-    // Add progress bar and output slot
-    const progressBarDiv = document.createElement("div")
-    progressBarDiv.className = "processor_progress_bar"
-    progressBarDiv.innerHTML = `
-      <div class='processor_progress_bar_container'>
-        <div class='processor_progress_bar_fill'></div>
-      </div>
-      <div class='arrow-right'></div>
-    `
-    storageDiv.appendChild(progressBarDiv)
-
-    // Output slot (always index = slotCount)
-    const outputDiv = document.createElement("div")
-    outputDiv.className = "output_inventory inventory_slot"
-    outputDiv.dataset.index = slotCount
-    outputDiv.innerHTML = "<img src=''>"
-    storageDiv.appendChild(outputDiv)
-
-    // Set up rest of menu
-    this.el.querySelector(".processor_storage").dataset.storageId = this.storageId
     this.el.querySelector(".menu_main_header").innerText = i18n.t(header)
     this.el.querySelector(".menu_description").innerText = i18n.t(description)
 
     if (options.disabled) {
       this.isDisabled = true
-      this.el.querySelector(".processor_status_message").innerText = i18n.t(options.disabled)
+      this.el.querySelector(".processor_status_message").innerText =
+        i18n.t(options.disabled)
     } else {
       this.isDisabled = false
       this.el.querySelector(".processor_status_message").innerText = ""
+    }
+
+    const storageDiv = this.el.querySelector(".processor_storage")
+    storageDiv.dataset.storageId = this.storageId
+    storageDiv.innerHTML = ""
+
+    const slotCount = entity.getConstants().storageCount
+
+    for (let i = 0; i < slotCount; i++) {
+      const slot = document.createElement("div")
+      slot.className = "inventory_slot"
+      slot.dataset.index = i
+      slot.innerHTML = "<img src=''>"
+
+      storageDiv.appendChild(slot)
+      this.initInventorySlotListener(slot)
+      slot.addEventListener("click", this.onInventoryClick.bind(this), true)
+
+      // Insert progress bar before final output slot
+      if (i === slotCount - 2) {
+        const bar = document.createElement("div")
+        bar.className = "processor_progress_bar"
+        bar.innerHTML = `
+          <div class='processor_progress_bar_container'>
+            <div class='processor_progress_bar_fill'></div>
+          </div>
+          <div class='arrow-right'></div>
+        `
+        storageDiv.appendChild(bar)
+      }
     }
 
     SocketUtil.emit("ViewStorage", { id: this.storageId })
 
     this.initPlayerInventoryStorage()
     this.initPlayerInventorySlotListeners()
+
+    // cache progress bar AFTER it exists
+    this.progressBar = this.el.querySelector(".processor_progress_bar_fill")
   }
 
   shouldHideQuickInventory() {
@@ -120,18 +117,20 @@ class ProcessorMenu extends BaseMenu {
   }
 
   cleanup() {
-    this.el.querySelector(".processor_storage").dataset.storageId = ""
+    const storageDiv = this.el.querySelector(".processor_storage")
+    storageDiv.dataset.storageId = ""
     this.storageId = null
 
-    Array.from(this.el.querySelectorAll(".inventory_slot")).forEach((inventorySlot) => {
-      this.game.resetInventorySlot(inventorySlot)
+    Array.from(this.el.querySelectorAll(".inventory_slot")).forEach(slot => {
+      this.game.resetInventorySlot(slot)
     })
 
-    this.progressBar.style.width = "0px"
+    if (this.progressBar) {
+      this.progressBar.style.width = "0px"
+    }
+
+    this.progressBar = null
   }
-
 }
-
-
 
 module.exports = ProcessorMenu
